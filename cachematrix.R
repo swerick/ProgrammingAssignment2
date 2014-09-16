91ba58059338a28a48cb60df07e4ff7fa6b30827
swerick <seanwerick@gmail.com>
2014-09-16 18:33:46 -0400

    Updated MakeCacheMatrix and cacheSolve function
    These functions were modified to correctly implement the homework
    assignment. The MakeCacheMatrix function now correctly caches a matrix
    while cacheSolve either solves a matrix or retrieves a value that has
    already been cached.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## MakeCacheMatrix  caches a matrix in physical memory
+##      eg y <- MakeCacheMatrix(matrix(c(1,2,3,4), 2, 2))
+## $get() retrieves the object value        eg (y$get())
+## $getmatrix() retrieves the object value  eg (y$getmatrix())
+## $setmatrix() sets the object value       eg (y$setmatrix(matrix(c(1,2,3,4), 2, 2)))
+## $set() sets the object value             eg (y$set(matrix(c(1,2,3,4), 2, 2)))
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## m is the free variable set in the parent environment
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
+  ## End Old Code
+  
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes a solvable matrix as input and 
+##    returns its inverse using the solve() function
+## This value is cached to possibly be used again
+## If the same matrix is passed to the cacheSolve 
+##    function, the cached value is retrieved, otherwise 
+##    the new matrix is cached and solved
+##    eg (cacheSolve(y)) where y is a solvable matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getmatrix()
+  if(!is.null(m)) {
+    message("getting cached matrix")
+    return(m)
+  }
+  thematrix <- x$get()
+  m <- solve(thematrix, ...)
+  x$setmatrix(m)
+  m
+  ## End Old Code
 }
